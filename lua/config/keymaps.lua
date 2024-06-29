c434cb4dad2727b23a4737b4ed8ddfc5e47bded0
Cuboid <fanjas112358@gmail.com>
2024-06-29 13:16:07 -0400

    recursive keymaps + working fold line
    x -> "_x is now recursive and actually used to prevent the other keymaps from becoming a clusterfuck when I repeatedly use "_x instead of x
    
    uh and I fixed the fold line/move next line to end of current line thing so it includes whitespace

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -158,63 +158,66 @@ end
 
 leader_1 = "," -- leader key go brrrr
 
-vim.keymap.set("n", "<C-a>", "gg_vG$", {noremap=true, desc="Select All"})
-vim.keymap.set("i", "<C-a>", "<Esc>gg_vG$", {noremap=true, desc="Select All"})
+vim.keymap.set("n", "x", "\"_x", {remap=false, desc="Delete"}) -- the x key no longer goes to any register
+vim.keymap.set("v", "x", "\"_x", {remap=false, desc="Delete"})
 
-vim.keymap.set("n", leader_1 .. "l", "v0\"_x", {noremap=true, desc="Delete everything before cursor in line"})
-vim.keymap.set("i", leader_1 .. "l", "<Esc>v0\"_xi", {noremap=true, desc="Delete everything before cursor in line"})
+vim.keymap.set("n", "<C-a>", "gg_vG$", {remap=true, desc="Select All"})
+vim.keymap.set("i", "<C-a>", "<Esc>gg_vG$", {remap=true, desc="Select All"})
 
-vim.keymap.set("n", leader_1 .. "x", "$a-<Esc>v0\"_xa<Backspace><Esc>", {noremap=true, desc="Delete Line"}) -- does some stuff with leaving the cursor in the right place which is why it's better than dd
-vim.keymap.set("i", leader_1 .. "x", "<Esc>$a-<Esc>v0\"_xa<Backspace>", {noremap=true, desc="Delete Line"})
+vim.keymap.set("n", leader_1 .. "l", "v0x", {remap=true, desc="Delete everything before cursor in line"})
+vim.keymap.set("i", leader_1 .. "l", "<Esc>v0xi", {remap=true, desc="Delete everything before cursor in line"})
 
-vim.keymap.set("n", leader_1 .. "n", "$a<Enter><Esc>", {noremap=true, desc="Create newline under"})
-vim.keymap.set("i", leader_1 .. "n", "<Esc>$a<Enter>", {noremap=true, desc="Create newline under"})
+vim.keymap.set("n", leader_1 .. "x", "$a-<Esc>v0xa<Backspace><Esc>", {remap=true, desc="Delete Line"}) -- does some stuff with leaving the cursor in the right place which is why it's better than dd
+vim.keymap.set("i", leader_1 .. "x", "<Esc>$a-<Esc>v0xa<Backspace>", {remap=true, desc="Delete Line"})
 
-vim.keymap.set("n", leader_1 .. "v", "$v_\"_x", {noremap=true, desc="Clear line"})
-vim.keymap.set("i", leader_1 .. "v", "<Esc>$v_\"_xa", {noremap=true, desc="Clear line"})
+vim.keymap.set("n", leader_1 .. "n", "$a<Enter><Esc>", {remap=true, desc="Create newline under"})
+vim.keymap.set("i", leader_1 .. "n", "<Esc>$a<Enter>", {remap=true, desc="Create newline under"})
 
-vim.keymap.set("n", leader_1 .. "b", "$v_y$a<Enter><Esc>p$", {noremap=true, desc="Copy and Paste Line"})
-vim.keymap.set("i", leader_1 .. "b", "<Esc>$v_y$a<Enter><Esc>p$a", {noremap=true, desc="Copy and Paste Line"})
+vim.keymap.set("n", leader_1 .. "v", "$v_x", {remap=true, desc="Clear line"})
+vim.keymap.set("i", leader_1 .. "v", "<Esc>$v_xa", {remap=true, desc="Clear line"})
 
-vim.keymap.set("n", "x", "\"_x", {noremap=true, desc="Delete"}) -- the x key no longer goes to any register
-vim.keymap.set("v", "x", "\"_x", {noremap=true, desc="Delete"})
+vim.keymap.set("n", leader_1 .. "b", "$v_y$a<Enter><Esc>p$", {remap=true, desc="Copy and Paste Line"})
+vim.keymap.set("i", leader_1 .. "b", "<Esc>$v_y$a<Enter><Esc>p$a", {remap=true, desc="Copy and Paste Line"})
 
-vim.keymap.set("n", leader_1 .. "p", "p", {noremap=true, desc="Paste"})
-vim.keymap.set("i", leader_1 .. "p", "<Esc>pa", {noremap=true, desc="Paste"})
+vim.keymap.set("n", leader_1 .. "p", "p", {remap=true, desc="Paste"})
+vim.keymap.set("i", leader_1 .. "p", "<Esc>pa", {remap=true, desc="Paste"})
 
-vim.keymap.set("n", leader_1 .. "ii", function() return "gg" .. last_import({ "import" }) .. "$" end, {noremap=true, desc="Go to last import", expr=true}) -- python funni go brrrr
-vim.keymap.set("i", leader_1 .. "ii", function() return "gg" .. last_import({ "import" }) .. "$a" end, {noremap=true, desc="Go to last import", expr=true})
-vim.keymap.set("n", leader_1 .. "if", function() return "gg" .. last_import({ "import", "from" }) .. "$" end, {noremap=true, desc="Go to last import", expr=true})
-vim.keymap.set("i", leader_1 .. "if", function() return "gg" .. last_import({ "import", "from" }) .. "$a" end, {noremap=true, desc="Go to last import", expr=true})
+vim.keymap.set("n", leader_1 .. "j", "j_\"qy$$v0xa<Backspace><Esc>\"qp", {remap=true, desc="Move next line to end of current line"})
+vim.keymap.set("i", leader_1 .. "j", "<Esc>j_\"qy$$v0xa<Backspace><Esc>\"qpa", {remap=true, desc="Move next line to end of current line"})
 
-vim.keymap.set("i", leader_1 .. "a", function() return "<Esc>mqv_\"qdgg" .. autoimport() .. "import <Esc>\"qp`q$:delm q<Enter>a" end, {noremap=true, desc="Auto import last word", expr=true})
+vim.keymap.set("n", leader_1 .. "ii", function() return "gg" .. last_import({ "import" }) .. "$" end, {remap=true, desc="Go to last import", expr=true}) -- python funni go brrrr
+vim.keymap.set("i", leader_1 .. "ii", function() return "gg" .. last_import({ "import" }) .. "$a" end, {remap=true, desc="Go to last import", expr=true})
+vim.keymap.set("n", leader_1 .. "if", function() return "gg" .. last_import({ "import", "from" }) .. "$" end, {remap=true, desc="Go to last import", expr=true})
+vim.keymap.set("i", leader_1 .. "if", function() return "gg" .. last_import({ "import", "from" }) .. "$a" end, {remap=true, desc="Go to last import", expr=true})
 
-vim.keymap.set("i", ")", function() return bracket(")") end, {noremap=true, expr=true}) -- auto go into parentheses/bracket/braces
-vim.keymap.set("i", "]", function() return bracket("]") end, {noremap=true, expr=true})
-vim.keymap.set("i", "}", function() return bracket("}") end, {noremap=true, expr=true})
--- vim.keymap.set("i", "\"", function() return bracket("\"") end, {noremap=true, expr=true})
+vim.keymap.set("i", leader_1 .. "a", function() return "<Esc>mqv_\"qdgg" .. autoimport() .. "import <Esc>\"qp`q$:delm q<Enter>a" end, {remap=true, desc="Auto import last word", expr=true})
+
+vim.keymap.set("i", ")", function() return bracket(")") end, {remap=true, expr=true}) -- auto go into parentheses/bracket/braces
+vim.keymap.set("i", "]", function() return bracket("]") end, {remap=true, expr=true})
+vim.keymap.set("i", "}", function() return bracket("}") end, {remap=true, expr=true})
+-- vim.keymap.set("i", "\"", function() return bracket("\"") end, {remap=true, expr=true})
 -- ^ this one screws up python """
 
-vim.keymap.set("n", leader_1 .. "c", "_i#<Esc>", {noremap=true, desc="Comment"})
-vim.keymap.set("i", leader_1 .. "c", "<Esc>_i#", {noremap=true, desc="Comment"})
-vim.keymap.set("v", leader_1 .. "c", function() return comment() end, {noremap=true, desc="Block Comment", expr=true})
+vim.keymap.set("n", leader_1 .. "c", "_i#<Esc>", {remap=true, desc="Comment"})
+vim.keymap.set("i", leader_1 .. "c", "<Esc>_i#", {remap=true, desc="Comment"})
+vim.keymap.set("v", leader_1 .. "c", function() return comment() end, {remap=true, desc="Block Comment", expr=true})
 
-vim.keymap.set("n", leader_1 .. "u", "_<C-v>:s/#//<Enter>", {noremap=true, desc="Uncomment"})
-vim.keymap.set("i", leader_1 .. "u", "<Esc>_<C-v>:s/#//<Enter>i", {noremap=true, desc="Uncomment"})
-vim.keymap.set("v", leader_1 .. "u", function() return uncomment() end, {noremap=true, desc="Block Uncomment", expr=true})
+vim.keymap.set("n", leader_1 .. "u", "_<C-v>:s/#//<Enter>", {remap=true, desc="Uncomment"})
+vim.keymap.set("i", leader_1 .. "u", "<Esc>_<C-v>:s/#//<Enter>i", {remap=true, desc="Uncomment"})
+vim.keymap.set("v", leader_1 .. "u", function() return uncomment() end, {remap=true, desc="Block Uncomment", expr=true})
 
-vim.keymap.set("n", leader_1 .. "m", "vbx", {noremap=true, desc="Delete last word"})
-vim.keymap.set("i", leader_1 .. "m", "<Esc>vbxa", {noremap=true, desc="Delete last word"})
+vim.keymap.set("n", leader_1 .. "m", "vbx", {remap=true, desc="Delete last word"})
+vim.keymap.set("i", leader_1 .. "m", "<Esc>vbxa", {remap=true, desc="Delete last word"})
 
-vim.keymap.set("n", "<Home>", "_", {noremap=true, desc="Go home, after indents"})
-vim.keymap.set("i", "<Home>", "<Esc>_i", {noremap=true, desc="Go home, after indents"})
-vim.keymap.set("v", "<Home>", "_", {noremap=true, desc="Go home, after indents"})
+vim.keymap.set("n", "<Home>", "_", {remap=true, desc="Go home, after indents"})
+vim.keymap.set("i", "<Home>", "<Esc>_i", {remap=true, desc="Go home, after indents"})
+vim.keymap.set("v", "<Home>", "_", {remap=true, desc="Go home, after indents"})
 
-vim.keymap.set("n", "<F5>", ":w<Enter>:exec '!start cmd /C pythonp' shellescape(@%, 1)<Enter>", {noremap=true, desc="Run Python"})
-vim.keymap.set("i", "<F5>", "<Esc>:w<Enter>:exec '!start cmd /C pythonp' shellescape(@%, 1)<Enter>a", {noremap=true, desc="Run Python"})
+vim.keymap.set("n", "<F5>", ":w<Enter>:exec '!start cmd /C pythonp' shellescape(@%, 1)<Enter>", {remap=true, desc="Run Python"})
+vim.keymap.set("i", "<F5>", "<Esc>:w<Enter>:exec '!start cmd /C pythonp' shellescape(@%, 1)<Enter>a", {remap=true, desc="Run Python"})
 
-vim.keymap.set("n", "<F9>", ":tabp<Enter>", {noremap=true, desc="Previous Tab"})
-vim.keymap.set("i", "<F9>", "<Esc>:tabp<Enter>a", {noremap=true, desc="Previous Tab"})
+vim.keymap.set("n", "<F9>", ":tabp<Enter>", {remap=true, desc="Previous Tab"})
+vim.keymap.set("i", "<F9>", "<Esc>:tabp<Enter>a", {remap=true, desc="Previous Tab"})
 
-vim.keymap.set("n", "<F10>", ":tabn<Enter>", {noremap=true, desc="Next Tab"})
-vim.keymap.set("i", "<F10>", "<Esc>:tabn<Enter>a", {noremap=true, desc="Next Tab"})
+vim.keymap.set("n", "<F10>", ":tabn<Enter>", {remap=true, desc="Next Tab"})
+vim.keymap.set("i", "<F10>", "<Esc>:tabn<Enter>a", {remap=true, desc="Next Tab"})
