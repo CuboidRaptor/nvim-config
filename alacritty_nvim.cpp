ae94d0f55e2eb8ed7e125f4a32b01a2d2ca9046f
Cuboid <fanjas112358@gmail.com>
2024-07-05 22:26:34 -0400

    moved alacritty
    actually having alacritty on PATH instead of keeping a portable copy is probably a pretty non-stupid idea

--- alacritty_nvim.cpp
+++ alacritty_nvim.cpp
@@ -42,14 +42,13 @@ int main(int argc, char *argv[]) {
         arguments = "";
     }
     
-    std::string start = "start ";
     std::string current_script_dir = GetExeDirectory().string();
-    std::string command = "alacritty --title \"NeoVim - Powered by Alacritty\"";
+    std::string command = "start alacritty --title \"NeoVim - Powered by Alacritty\"";
     std::string config_command = " --config-file \"";
     config_command += current_script_dir + "alacritty-config/alacritty.toml\"";
     std::string nvim_command = std::string(" -e \"nvim ") + arguments + "\"";
     
-    std::string output = start + current_script_dir + command + config_command + nvim_command;
+    std::string output = command + config_command + nvim_command;
     
     std::cout << output;
     system(output.c_str());
